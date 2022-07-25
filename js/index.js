1f90af174ed56eec517f236e87300faa4968272c
ZacharyJSwatfigure <zacharyswatfigure@gmail.com>
2022-07-25 10:21:14 -0600

    working on turn one for the computer

--- js/index.js
+++ js/index.js
@@ -10,6 +10,15 @@ var middleR = document.getElementById('middleR');
 var bottomL = document.getElementById('bottomL');
 var bottomM = document.getElementById('bottomM');
 var bottomR = document.getElementById('bottomR');
+var topLActive = null;
+var topMActive = null;
+var topRActive = null;
+var middleLActive = null;
+var middleMActive = null;
+var middleRActive = null;
+var bottomLActive = null;
+var bottomMActive = null;
+var bottomRActive = null;
 var xo = document.getElementsByClassName('xo');
 // 0 === false === comp turn
 // 1 === true === player turn
@@ -17,13 +26,12 @@ var whoStarts = function () {
     var randomNum = Math.floor(Math.random() * 2);
     if (randomNum === 0) {
         console.log(randomNum);
+        computerTurn();
         return turn = false;
     }
     else {
         console.log(randomNum);
-        turn = true;
-        computerTurn();
-        return;
+        return turn = true;
     }
 };
 var changeTurnBox = function () {
@@ -49,40 +57,154 @@ var computerTurnOne = function () {
                 case 1:
                     topL.style.backgroundColor = "red";
                     turnCounter = turnCounter + 1;
-                    turn = false;
+                    turn = true;
                     break;
                 case 2:
                     topR.style.backgroundColor = "red";
                     turnCounter = turnCounter + 1;
-                    turn = false;
+                    turn = true;
                     break;
                 case 3:
                     bottomL.style.backgroundColor = "red";
                     turnCounter = turnCounter + 1;
-                    turn = false;
+                    turn = true;
                     break;
                 case 4:
                     bottomR.style.backgroundColor = "red";
                     turnCounter = turnCounter + 1;
-                    turn = false;
+                    turn = true;
                     break;
             }
         }
     }
 };
+var updatePosition = function () {
+    if (turnCounter >= 1) {
+        if (topL.style.backgroundColor != "white") {
+            if (topL.style.backgroundColor === "red") {
+                topLActive = false;
+            }
+            else if (topL.style.backgroundColor === "green") {
+                topLActive = true;
+            }
+        }
+        if (topM.style.backgroundColor != "white") {
+            if (topM.style.backgroundColor === "red") {
+                topMActive = false;
+            }
+            else if (topM.style.backgroundColor === "green") {
+                topMActive = true;
+            }
+        }
+        if (topR.style.backgroundColor != "white") {
+            if (topR.style.backgroundColor === "red") {
+                topMActive = false;
+            }
+            else if (topR.style.backgroundColor === "green") {
+                topRActive = true;
+            }
+        }
+        if (middleL.style.backgroundColor != "white") {
+            if (middleL.style.backgroundColor === "red") {
+                middleLActive = false;
+            }
+            else if (middleL.style.backgroundColor === "green") {
+                middleLActive = true;
+            }
+        }
+        if (middleM.style.backgroundColor != "white") {
+            if (middleM.style.backgroundColor === "red") {
+                middleMActive = false;
+            }
+            else if (middleM.style.backgroundColor === "green") {
+                middleMActive = true;
+            }
+        }
+        if (middleR.style.backgroundColor != "white") {
+            if (middleR.style.backgroundColor === "red") {
+                middleRActive = false;
+            }
+            else if (middleR.style.backgroundColor === "green") {
+                middleRActive = true;
+            }
+        }
+        if (bottomL.style.backgroundColor != "white") {
+            if (bottomL.style.backgroundColor === "red") {
+                bottomLActive = false;
+            }
+            else if (bottomL.style.backgroundColor === "green") {
+                bottomLActive = true;
+            }
+        }
+        if (bottomM.style.backgroundColor != "white") {
+            if (bottomM.style.backgroundColor === "red") {
+                bottomMActive = false;
+            }
+            else if (bottomM.style.backgroundColor === "green") {
+                bottomMActive = true;
+            }
+        }
+        if (bottomR.style.backgroundColor != "white") {
+            if (bottomR.style.backgroundColor === "red") {
+                bottomRActive = false;
+            }
+            else if (bottomR.style.backgroundColor === "green") {
+                bottomRActive = true;
+            }
+        }
+        else {
+            return console.log("gameover? 152");
+        }
+    }
+};
+var noCorners = function () {
+    console.log("noCorners");
+};
 var computerTurn = function () {
     if (turnCounter === 0) {
         computerTurnOne();
     }
-    if (turnCounter >= 1) {
+    else if (turnCounter > 0) {
+        updatePosition();
+        if (!topLActive) {
+            if (!topRActive) {
+                if (!bottomLActive) {
+                    if (!bottomRActive) {
+                        noCorners();
+                    }
+                    else {
+                        bottomR.style.backgroundColor = "red";
+                        bottomRActive = false;
+                    }
+                }
+                else {
+                    bottomL.style.backgroundColor = "red";
+                    bottomRActive = false;
+                }
+            }
+            else {
+                topR.style.backgroundColor = "red";
+                topLActive = false;
+            }
+        }
+        else {
+            topL.style.backgroundColor = "red";
+            topLActive = false;
+        }
     }
 };
+//start writing here
 var boxClicked = function (boxPos) {
     if (turn) {
-        boxPos.style.backgroundColor = "green";
-        turnCounter += 1;
-        turn = false;
-        return computerTurn();
+        if (boxPos.style.backgroundColor != "red" && boxPos.style.backgroundColor != "green") {
+            boxPos.style.backgroundColor = "green";
+            turnCounter += 1;
+            computerTurn();
+            return turn = false;
+        }
+        else {
+            return console.log("error. section already selected");
+        }
     }
     else if (!turn) {
         return console.log("not your turn, Goober");
--- js/index.ts
+++ js/index.ts
@@ -17,6 +17,17 @@ let bottomL = document.getElementById('bottomL');
 let bottomM = document.getElementById('bottomM');
 let bottomR = document.getElementById('bottomR');
 
+let topLActive: any = null;
+let topMActive: any = null;
+let topRActive: any = null;
+let middleLActive: any = null;
+let middleMActive: any = null;
+let middleRActive: any = null;
+let bottomLActive: any = null;
+let bottomMActive: any = null;
+let bottomRActive: any = null;
+
+
 let xo = document.getElementsByClassName('xo');
 
 // 0 === false === comp turn
@@ -26,11 +37,11 @@ const whoStarts = () => {
   const randomNum: number = Math.floor(Math.random() * 2);
   if (randomNum === 0) {
     console.log(randomNum)
+    computerTurn();
     return turn = false;
   } else {
     console.log(randomNum)
-    turn = true;
-    computerTurn()
+    return turn = true;
   }
 }
 
@@ -55,22 +66,22 @@ const computerTurnOne = () => {
         case 1:
           topL.style.backgroundColor = "red"
           turnCounter = turnCounter + 1;
-          turn = false;
+          turn = true;
           break;
         case 2:
           topR.style.backgroundColor = "red"
           turnCounter = turnCounter + 1;
-          turn = false;
+          turn = true;
           break;
         case 3:
           bottomL.style.backgroundColor = "red"
           turnCounter = turnCounter + 1;
-          turn = false;
+          turn = true;
           break;
         case 4:
           bottomR.style.backgroundColor = "red"
           turnCounter = turnCounter + 1;
-          turn = false;
+          turn = true;
           break;
 
       }
@@ -78,27 +89,120 @@ const computerTurnOne = () => {
   }
 }
 
+const updatePosition = () => {
+  if (turnCounter >= 1) {
+    if (topL.style.backgroundColor != "white") {
+      if (topL.style.backgroundColor === "red") {
+        topLActive = false;
+      } else if (topL.style.backgroundColor === "green") {
+        topLActive = true;
+      }
+    }if (topM.style.backgroundColor != "white") {
+      if (topM.style.backgroundColor === "red") {
+        topMActive = false;
+      } else if (topM.style.backgroundColor === "green") {
+        topMActive = true;
+      }
+    }if (topR.style.backgroundColor != "white") {
+      if (topR.style.backgroundColor === "red") {
+        topMActive = false;
+      } else if (topR.style.backgroundColor === "green") {
+        topRActive = true;
+      }
+    }if (middleL.style.backgroundColor != "white") {
+      if (middleL.style.backgroundColor === "red") {
+        middleLActive = false;
+      } else if (middleL.style.backgroundColor === "green") {
+        middleLActive = true;
+      }
+    }if (middleM.style.backgroundColor != "white") {
+      if (middleM.style.backgroundColor === "red") {
+        middleMActive = false;
+      } else if (middleM.style.backgroundColor === "green") {
+        middleMActive = true;
+      }
+    }if (middleR.style.backgroundColor != "white") {
+      if (middleR.style.backgroundColor === "red") {
+        middleRActive = false;
+      } else if (middleR.style.backgroundColor === "green") {
+        middleRActive = true;
+      }
+    }if (bottomL.style.backgroundColor != "white") {
+      if (bottomL.style.backgroundColor === "red") {
+        bottomLActive = false;
+      } else if (bottomL.style.backgroundColor === "green") {
+        bottomLActive = true;
+      }
+    }if (bottomM.style.backgroundColor != "white") {
+      if (bottomM.style.backgroundColor === "red") {
+        bottomMActive = false;
+      } else if (bottomM.style.backgroundColor === "green") {
+        bottomMActive = true;
+      }
+    }if (bottomR.style.backgroundColor != "white") {
+      if (bottomR.style.backgroundColor === "red") {
+        bottomRActive = false;
+      } else if (bottomR.style.backgroundColor === "green") {
+        bottomRActive = true;
+      }
+    } else {
+      return console.log("gameover? 152")
+    }
+  }
+}
+
+const noCorners = () => {
+  console.log("noCorners")
+}
+
 const computerTurn = () => {
   if (turnCounter === 0) {
     computerTurnOne();
-  }
-  if (turnCounter >= 1) {
-
+  } else if (turnCounter > 0) {
+    updatePosition();
+    if (!topLActive) {
+      if (!topRActive){
+        if (!bottomLActive) {
+          if (!bottomRActive) {
+            noCorners();
+          }else {
+            bottomR.style.backgroundColor = "red"
+            bottomRActive = false;
+          }
+        }else {
+          bottomL.style.backgroundColor = "red"
+          bottomRActive = false
+        }
+      }else {
+        topR.style.backgroundColor = "red"
+        topLActive = false;
+      }
+    } else {
+      topL.style.backgroundColor = "red"
+      topLActive = false;
+    }
   }
 }
+
+ 
+  
+  //start writing here
 
   
 const boxClicked = (boxPos) => {
   
   if (turn) {
-    boxPos.style.backgroundColor = "green"
-    turnCounter += 1;
-    turn = false;
-    return computerTurn();
+    if (boxPos.style.backgroundColor != "red" && boxPos.style.backgroundColor != "green") {
+      boxPos.style.backgroundColor = "green"
+      turnCounter += 1;
+      computerTurn();
+      return turn = false;
+    } else {
+      return console.log("error. section already selected")
+    }
   } else if (!turn) {
     return console.log("not your turn, Goober")
   }
-  
 }
 
 const runGame = () => {
